29fab772977e220ba05837afa992edaf5fab5a25
filip-wojc <filipwojcik203@gmail.com>
2025-03-18 22:56:37 +0100

    move friend-request endpoints to FriendController

---
+++ SignaliteWebAPI/Controllers/FriendsController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SignaliteWebAPI.Application.Features.Users.GetFriendRequests;
+using SignaliteWebAPI.Application.Features.Users.SendFriendRequest;
+using SignaliteWebAPI.Domain.DTOs.FriendRequests;
+
+namespace SignaliteWebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FriendsController(ISender mediator) : ControllerBase
+{
+    [HttpPost("friend-request")]
+    public async Task<IActionResult> SendFriendRequest(SendFriendRequestCommand sendFriendRequestCommand)
+    {
+        await mediator.Send(sendFriendRequestCommand);
+        return Created();
+    }
+
+    // Will be replaced with user id from token
+    [HttpGet("friend-requests/{userId}")]
+    public async Task<ActionResult<List<FriendRequestDTO>>> GetFriendRequests([FromRoute] int userId)
+    {
+        var friendRequests = await mediator.Send(new GetFriendRequestsQuery { UserId = userId });
+        return Ok(friendRequests);
+    }
+}
