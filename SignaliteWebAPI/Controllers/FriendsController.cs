0ed51ce154e62da9056e457f39e10df8e65725e0
filip-wojc <filipwojcik203@gmail.com>
2025-03-20 11:19:44 +0100

    extension method for ClaimsPrincipal and getting userId from token in FriendsController.cs

--- SignaliteWebAPI/Controllers/FriendsController.cs
+++ SignaliteWebAPI/Controllers/FriendsController.cs
@@ -1,27 +1,34 @@
-﻿using MediatR;
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SignaliteWebAPI.Application.Features.Users.GetFriendRequests;
 using SignaliteWebAPI.Application.Features.Users.SendFriendRequest;
 using SignaliteWebAPI.Domain.DTOs.FriendRequests;
+using SignaliteWebAPI.Extensions;
 
 namespace SignaliteWebAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class FriendsController(ISender mediator) : ControllerBase
 {
-    [HttpPost("friend-request")]
-    public async Task<IActionResult> SendFriendRequest(SendFriendRequestCommand sendFriendRequestCommand)
+    [HttpPost("friend-request/{recipientId}")]
+    public async Task<IActionResult> SendFriendRequest([FromRoute] int recipientId)
     {
-        await mediator.Send(sendFriendRequestCommand);
+        var command = new SendFriendRequestCommand
+        {
+            SendFriendRequestDTO = new SendFriendRequestDTO { RecipientId = recipientId, SenderId = User.GetUserId() }
+        };
+        await mediator.Send(command);
         return Created();
     }
 
-    // Will be replaced with user id from token
-    [HttpGet("friend-requests/{userId}")]
-    public async Task<ActionResult<List<FriendRequestDTO>>> GetFriendRequests([FromRoute] int userId)
+    [HttpGet("friend-requests")]
+    public async Task<ActionResult<List<FriendRequestDTO>>> GetFriendRequests()
     {
-        var friendRequests = await mediator.Send(new GetFriendRequestsQuery { UserId = userId });
+        var friendRequests = await mediator.Send(new GetFriendRequestsQuery { UserId = User.GetUserId() });
         return Ok(friendRequests);
     }
 }
---
+++ SignaliteWebAPI/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using SignaliteWebAPI.Application.Exceptions;
+
+namespace SignaliteWebAPI.Extensions;
+
+public static class ClaimsPrincipleExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+            throw new AuthException("Cannot get id from token");
+        }
+        return int.Parse(userId);
+    }
+}
