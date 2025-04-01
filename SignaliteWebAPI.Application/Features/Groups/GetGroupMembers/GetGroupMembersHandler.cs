3827c9be4f3381ed2de9e4d14b1fb9b8a6443789
filip-wojc <filipwojcik203@gmail.com>
2025-04-01 16:00:59 +0200

    get groups, group basic info and group members

---
+++ SignaliteWebAPI.Application/Features/Groups/GetGroupMembers/GetGroupMembersHandler.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MediatR;
+using SignaliteWebAPI.Domain.DTOs.Groups;
+using SignaliteWebAPI.Infrastructure.Interfaces.Repositories;
+
+namespace SignaliteWebAPI.Application.Features.Groups.GetGroupMembers;
+
+public class GetGroupMembersHandler(IGroupRepository groupRepository, IMapper mapper) : IRequestHandler<GetGroupMembersQuery, GroupMembersDTO>
+{
+    public async Task<GroupMembersDTO> Handle(GetGroupMembersQuery request, CancellationToken cancellationToken)
+    {
+        var groupWithMembers = await groupRepository.GetGroupMembers(request.GroupId);
+        return mapper.Map<GroupMembersDTO>(groupWithMembers);
+    }
+}
