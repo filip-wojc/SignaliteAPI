3e13ac92d2628b36b06e02659c0157eb6b2c4ee9
RobertPintera <robert.it.990@gmail.com>
2025-04-04 19:25:42 +0200

    Fix ModifyUserValidator - can change only name and surname

--- SignaliteWebAPI.Application/Features/Users/ModifyUser/ModifyUserValidator.cs
+++ SignaliteWebAPI.Application/Features/Users/ModifyUser/ModifyUserValidator.cs
@@ -25,21 +25,37 @@ public class ModifyUserValidator : AbstractValidator<ModifyUserCommand>
             .NotNull().WithMessage("Username is required.")
             .NotEmpty().WithMessage("Username is empty.")
             .Length(minLengthUsername, maxLengthUsername).WithMessage($"Username must be between {minLengthUsername} and {maxLengthUsername} characters.")
-            .MustAsync(IsUsernameUnique).WithMessage("Username is already taken");
+            .MustAsync((command, username, cancellationToken) => IsUsernameUnique(command.UserId, username, cancellationToken)).WithMessage("Username is already taken");
         
         RuleFor(u => u.ModifyUserDTO.Email)
             .NotNull().WithMessage("Email is required.")
             .NotEmpty().WithMessage("Email is empty.")
             .EmailAddress().WithMessage("Invalid email address.")
-            .MustAsync(IsEmailUnique).WithMessage("Email is already taken");
+            .MustAsync((command, email, cancellationToken) => IsEmailUnique(command.UserId, email, cancellationToken)).WithMessage("Email is already taken");
     }
     
-    private async Task<bool> IsUsernameUnique(string username, CancellationToken cancellationToken)
+    private async Task<bool> IsUsernameUnique(int userId, string username, CancellationToken cancellationToken)
     {
-        return await _userRepository.GetUserByUsername(username) == null;
+        var user = await _userRepository.GetUserByUsername(username);
+        
+        if (user == null)
+            return true;
+        
+        if (user.Id == userId)
+            return true;
+        
+        return false;
     }
-    private async Task<bool> IsEmailUnique(string email, CancellationToken cancellationToken)
+    private async Task<bool> IsEmailUnique(int userId, string email, CancellationToken cancellationToken)
     { 
-        return await _userRepository.GetUserByEmail(email) == null;
+        var user = await _userRepository.GetUserByEmail(email);
+        
+        if (user == null)
+            return true;
+        
+        if (user.Id == userId)
+            return true;
+        
+        return false;
     }
 }
