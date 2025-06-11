738ebbadde07f7929dfcbb0fb78939d3142d25b9
filip-wojc <filipwojcik203@gmail.com>
2025-06-11 13:14:09 +0200

    date sent add to messageDTO

--- SignaliteWebAPI.Domain/DTOs/Messages/MessageDTO.cs
+++ SignaliteWebAPI.Domain/DTOs/Messages/MessageDTO.cs
@@ -8,6 +8,7 @@ public class MessageDTO
     public int Id { get; set; }
     public string? Content { get; set; }
     public DateTime LastModification { get; set; }
+    public DateTime DateSent { get; set; }
     public AttachmentDTO? Attachment { get; set; }
     public UserBasicInfo Sender { get; set; }
 }
